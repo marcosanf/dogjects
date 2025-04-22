b6ae5530a5af5ae1df816a18ae433ded2e0cf5d2
Marcos Chaves <marcosanf.c@gmail.com>
2025-04-22 16:55:41 -0300

    style(app): setup app colors

---
+++ lib/ui/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xff13111C);
+  static const Color primary = Color(0xff4318D1);
+  static const Color white = Colors.white;
+  static const Color text = Colors.white;
+  static const Color error = Colors.red;
+  static const Color card = Colors.white;
+}
