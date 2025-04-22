12db744fd11b8f545ec3b25352196173a4e34c7d
Marcos Chaves <marcosanf.c@gmail.com>
2025-04-22 14:05:43 -0300

    refactor(dog-model): simplify model

--- lib/domain/dog.dart
+++ lib/domain/dog.dart
@@ -1,18 +1,13 @@
 class Dog {
-  String? message;
-  String? status;
+ final String imageUrl;
 
-  Dog({this.message, this.status});
+  Dog({required this.imageUrl});
 
-  Dog.fromJson(Map<String, dynamic> json) {
-    message = json['message'];
-    status = json['status'];
+  factory Dog.fromJson(Map<String, dynamic> json) {
+    return Dog(
+      imageUrl: json['message'],
+    );
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data =  Map<String, dynamic>();
-    data['message'] = message;
-    data['status'] = status;
-    return data;
-  }
+  
 }
