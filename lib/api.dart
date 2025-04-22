fe52b0d24a41e168c5aefccfe455697db4c04ca9
Marcos Chaves <marcosanf.c@gmail.com>
2025-04-22 11:14:12 -0300

    feat: move dog model to domain layer

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:desafio/model.dart';
+import 'package:desafio/domain/dog.dart';
 import 'package:http/http.dart' as http;
 
 
@@ -11,12 +11,8 @@ Future<Dog> fetchDog() async {
   );
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
     return Dog.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:desafio/api.dart';
-import 'package:desafio/model.dart';
+import 'package:desafio/domain/dog.dart';
 import 'package:flutter/material.dart';
 
 void main() {
